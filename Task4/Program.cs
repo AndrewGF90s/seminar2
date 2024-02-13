6495a7303c7418d6200f9b8847482598aa239e62
andy <andyggggg>
2024-02-13 22:15:16 +0300

    add Task4  HomeWork

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,10 +2,13 @@
 Console.WriteLine("Ведите число");
 int number = Convert.ToInt32(Console.ReadLine());
 int numberModifi = 0;
-while (number / 10 > 9 ) {
-    numberModifi = number % 10;
-    Console.Write(numberModifi + "," );
-}
+    while (number > 0) {
+                numberModifi = number % 10;
+                number = number / 10;
+        Console.Write(numberModifi+",");
+    }
+
+
 
 
 
