3cb9dd9a566f058a3cb3e8f972cbc1d7ea8ea16a
andy <andyggggg>
2024-02-13 21:30:39 +0300

    add Task2 adn HomeWork

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер
+//  координатной четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("Введите число X. X не может быть равен 0");
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число Y. Y не может быть равен 0");
+
+int y = Convert.ToInt32(Console.ReadLine());
+
+int spot =0;
+
+if(x != 0 && y != 0) {
+    if (x > 0 && y > 0) {
+    spot = 1;
+      Console.WriteLine($"координаты находятся в секторе {spot}");
+    }
+    else if (x > 0 && y < 0) {
+        spot = 4;
+         Console.WriteLine($"координаты находятся в секторе {spot}"); 
+    }
+    else if (x < 0 && y > 0) {
+        spot = 2;
+          Console.WriteLine($"координаты находятся в секторе {spot}");
+    }
+    else {
+        spot = 3;
+          Console.WriteLine($"координаты находятся в секторе {spot}");
+    }
+}
+else {
+    Console.WriteLine("Вы ввели некорректные значение");
+}
+
+
+
+
+
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+Console.WriteLine("Ведите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberModifi = 0;
+while (number / 10 > 9 ) {
+    numberModifi = number % 10;
+    Console.Write(numberModifi + "," );
+}
+
+
+
